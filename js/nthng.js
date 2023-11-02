caea6ef8343d1e7160aaf85fa55cc3f032f7f8fc
JANAKIRAMAN <janakiramankeerthivelan1021@gmail.com>
2023-11-02 19:46:13 +0530

    Updated front end backend is remaning

--- js/nthng.js
+++ js/nthng.js
@@ -1,3 +1,8 @@
-function nthng() {
-    window.alert("Ithulla Onnum Illa Ok click pannitu summa poda jobi 😂");
-}
+const sq = require("sqlite3").verbose()
+ 
+let db = new sqlite3.Database('usersdb', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the users database.');
+  });
---
+++ login.js
@@ -0,0 +1,5 @@
+function login() {
+    let email = document.getElementById("e_mail");
+    let pass = document.getElementById("pass_word");
+    window.alert(email.value,pass.value)
+}
