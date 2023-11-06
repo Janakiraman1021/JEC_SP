4ff0ffea49b63e1ea59496c1c0d8d2e8c837ce7b
JANAKIRAMAN <janakiramankeerthivelan1021@gmail.com>
2023-11-06 21:28:03 +0530

    login success

--- js/login.js
+++ js/login.js
@@ -33,3 +33,21 @@ document.getElementById("loginButton").addEventListener(redirect)
   }
 
   };
+
+
+
+
+function verify(){
+  let email = document.getElementById("email").value;
+  let pass = document.getElementById("password").value;
+  if (email=="janakiramankeerthivelan1021@gmail.com"){
+    if (pass == 110822205025){
+      window.alert("Hello Login Successfull");
+    }
+  }
+  else{
+    window.alert("Login Failured !")
+    window.alert(email);
+    window.alert(pass);
+  }
+}
